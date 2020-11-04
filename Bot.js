7f725f1168732fbea12e7898180bf0d244945de4
Silvestrs Kante <silvestrs@wizardsdc.com>
2020-11-04 19:32:11 +0000

    testing changes with colab

--- Bot.js
+++ Bot.js
@@ -1,42 +1,44 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-const prefix = '-';
+const prefix = "-";
 
-const fs = require('fs');
+const fs = require("fs");
 
-const token = require('./token')
+const token = require("./token");
 
 client.commands = new Discord.Collection();
- 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
- 
-    client.commands.set(command.name, command);
+const commandFiles = fs
+  .readdirSync("./commands/")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+
+  client.commands.set(command.name, command);
 }
 
-client.once('ready' , () => {
-    console.log('Among Us MMR Bot is Online!')
+client.once("ready", () => {
+  console.log("Among Us MMR Bot is Online!");
 });
 
-client.on('message', message =>{
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
- 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
- 
-    if(command === 'ping'){
-        client.commands.get('ping').execute(message, args);
-    } 
-    else if(command === 'match'){
-        client.commands.get('match').execute(message, args);
-    }
-});
- 
-client.login('   ');
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === "ping") {
+    client.commands.get("ping").execute(message, args);
+  } else if (command === "match") {
+    client.commands.get("match").execute(message, args);
+  }
+});
+
+client.login("   ");
+
+// This is my change
 
 client.login(token);
